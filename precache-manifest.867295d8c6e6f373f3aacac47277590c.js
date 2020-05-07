e8aea00ff70c088938158d99c7aef6102b750a42
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-07 10:28:47 +0000

    Deploy tks18/shan-tk-portfolio-react to github.com/tks18/shan-tk-portfolio-react.git:gh-pages

---
+++ precache-manifest.867295d8c6e6f373f3aacac47277590c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e5c761c1d7d5a4a89a3c48f40c0308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04d959ea4c8e28502a20",
+    "url": "/static/js/2.fda6bded.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fda6bded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa4a3938f95093fd06e",
+    "url": "/static/js/main.db3f9d4e.chunk.js"
+  },
+  {
+    "revision": "d99223a0d35dd18bbb83",
+    "url": "/static/js/runtime-main.49c3ddc4.js"
+  }
+]);
